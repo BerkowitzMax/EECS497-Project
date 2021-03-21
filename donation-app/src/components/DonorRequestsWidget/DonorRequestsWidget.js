7e14fb886d19d41492a5dc6efc56df4864d99bd3
Karina Rebecca Peters <karinape@umich.edu>
2021-03-20 22:00:55 -0400

    changed request list to accordion; installed bootstrap-icons

--- donation-app/src/components/DonorRequestsWidget/DonorRequestsWidget.js
+++ donation-app/src/components/DonorRequestsWidget/DonorRequestsWidget.js
@@ -1,91 +1,105 @@
-import DonorRequest from "@/components/DonorRequest/index.vue"
+import DonorRequest from "@/components/DonorRequest/index.vue";
 
 export default {
-	name: "DonorRequestsWidget",
-	components: {
-		DonorRequest,
-	},
-	props: {},
-	data() {
-		return {
-			pendingRequests: [
-				{
-					// Should come from database eventually
-					status: "Pending",
-					dateCreated: new Date(),
-					charityName: "Charity Name",
-					charityContact: "Charity Contact",
-					donationLabel: "Donation Label",
-					charityImage: "profile-image-placeholder.png",
-					formData: {}
-				},
-				{
-					status: "Pending",
-					dateCreated: new Date(),
-					charityName: "Charity Name",
-					charityContact: "Charity Contact",
-					donationLabel: "Donation Label",
-					charityImage: "profile-image-placeholder.png",
-					formData: {}
-				},
-				{
-					status: "Pending",
-					dateCreated: new Date(),
-					charityName: "Charity Name",
-					charityContact: "Charity Contact",
-					donationLabel: "Donation Label",
-					charityImage: "profile-image-placeholder.png",
-					formData: {}
-				}
-			],
-			resolvedRequests: [
-				{
-					status: "Accepted",
-					dateCreated: new Date(),
-					charityName: "Charity Name",
-					charityContact: "Charity Contact",
-					donationLabel: "Donation Label",
-					charityImage: "profile-image-placeholder.png",
-					formData: {}
-				},
-				{
-					status: "Accepted",
-					dateCreated: new Date(),
-					charityName: "Charity Name",
-					charityContact: "Charity Contact",
-					donationLabel: "Donation Label",
-					charityImage: "profile-image-placeholder.png",
-					formData: {}
-				},
-				{
-					status: "Rejected",
-					dateCreated: new Date(),
-					charityName: "Charity Name",
-					charityContact: "Charity Contact",
-					donationLabel: "Donation Label",
-					charityImage: "profile-image-placeholder.png",
-					formData: {}
-				},
-				{
-					status: "Rejected",
-					dateCreated: new Date(),
-					charityName: "Charity Name",
-					charityContact: "Charity Contact",
-					donationLabel: "Donation Label",
-					charityImage: "profile-image-placeholder.png",
-					formData: {}
-				}
-			]
-		};
-	},
-	computed: {},
-	mounted() {},
-	methods: {
-		addRequest() {
-			// TODO: retrieve request data from database
-			let request = {}
+  name: "DonorRequestsWidget",
+  components: {
+    DonorRequest,
+  },
+  props: {},
+  data() {
+    return {
+      pendingRequests: [
+        {
+          // Should come from database eventually
+          id: 0,
+          status: "Pending",
+          dateCreated: new Date(),
+          charityName: "Charity Name",
+          charityContact: "Charity Contact",
+          charityLocation: "Charity Location",
+          donationLabel: "Donation Label",
+          charityImage: "profile-image-placeholder.png",
+          formData: {},
+        },
+        {
+          id: 1,
+          status: "Pending",
+          dateCreated: new Date(),
+          charityName: "Charity Name",
+          charityContact: "Charity Contact",
+          charityLocation: "Charity Location",
+          donationLabel: "Donation Label",
+          charityImage: "profile-image-placeholder.png",
+          formData: {},
+        },
+        {
+          id: 2,
+          status: "Pending",
+          dateCreated: new Date(),
+          charityName: "Charity Name",
+          charityContact: "Charity Contact",
+          charityLocation: "Charity Location",
+          donationLabel: "Donation Label",
+          charityImage: "profile-image-placeholder.png",
+          formData: {},
+        },
+      ],
+      resolvedRequests: [
+        {
+          id: 3,
+          status: "Accepted",
+          dateCreated: new Date(),
+          charityName: "Charity Name",
+          charityContact: "Charity Contact",
+          charityLocation: "Charity Location",
+          donationLabel: "Donation Label",
+          charityImage: "profile-image-placeholder.png",
+          formData: {},
+        },
+        {
+          id: 4,
+          status: "Accepted",
+          dateCreated: new Date(),
+          charityName: "Charity Name",
+          charityContact: "Charity Contact",
+          charityLocation: "Charity Location",
+          donationLabel: "Donation Label",
+          charityImage: "profile-image-placeholder.png",
+          formData: {},
+        },
+        {
+          id: 5,
+          status: "Rejected",
+          dateCreated: new Date(),
+          charityName: "Charity Name",
+          charityContact: "Charity Contact",
+          charityLocation: "Charity Location",
+          donationLabel: "Donation Label",
+          charityImage: "profile-image-placeholder.png",
+          formData: {},
+        },
+        {
+          id: 6,
+          status: "Rejected",
+          dateCreated: new Date(),
+          charityName: "Charity Name",
+          charityContact: "Charity Contact",
+          charityLocation: "Charity Location",
+          donationLabel: "Donation Label",
+          charityImage: "profile-image-placeholder.png",
+          formData: {},
+        },
+      ],
+    };
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+    addRequest() {
+      // TODO: retrieve request data from database
+      let request = {};
 
-			this.requests.push(request)
-		}
-	},
+      this.requests.push(request);
+    },
+  },
 };
