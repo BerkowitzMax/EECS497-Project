530c5d5c6c1e1e5067ce73624123b7a5e8d45d7e
pgogri <pgogri@umich.edu>
2021-03-26 16:25:38 +0530

    added the test file for DonorStatsWidget to hold future tests.

---
+++ donation-app/tests/unit/DonorStatsWidget.spec.js
@@ -0,0 +1,8 @@
+import DonorStatsWidget from "@/components/DonorStatsWidget/index.vue";
+import "@testing-library/jest-dom";
+
+describe("DonorStatsWidget", () => {
+    it("should do something", () => {
+      // TODO
+    });
+  });
