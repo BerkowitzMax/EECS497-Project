a49763147351d0e03676522a5c1ea8857d91e6ed
maxberko <maxberko@umich.edu>
2021-03-27 11:32:38 -0400

    log in as charity or donor, togglable, redirects page

--- donation-app/src/views/LandingPage/LandingPage.js
+++ donation-app/src/views/LandingPage/LandingPage.js
@@ -15,21 +15,27 @@ export default {
 	mounted() {},
 	methods: {
 		GoogleLogin: function() {
+			const user_type = document.getElementById("org").checked ? "Charities" : "Donors";
+
 			const provider = new firebase.auth.GoogleAuthProvider();
 			firebase.auth().signInWithPopup(provider)
-					.then(result => {
-						const user = result.user;
-						document.write(`Hello ${user.email}`);
-						console.log(user);
+				.then(result => {
+					const user = result.user;
+					console.log(user);
 
-						var path = user.email.split('@')[0];
-						db.collection("Donors").doc(path).set({
-							username: user.displayName,
-							email: user.email
-						});
+					var path = user.email.split('@')[0];
+					db.collection(user_type).doc(path).set({
+						username: user.displayName,
+						email: user.email
+					});
 
-					})
-					.catch(console.log)
+					// page redirect
+					if (user_type === "Donors")
+						this.$router.push({ name: 'donor-home'});
+					else
+						this.$router.push({ name: 'charity-home'});
+				})
+				.catch(console.log)
 		}
 	},
 };
