5e9408f6347a5c435274467a3bc050cf5729bb43
maxberko <maxberko@umich.edu>
2021-04-20 12:13:13 -0400

    fixed bug where pending requests prevented history from printing

--- donation-app/src/components/DonorRequestWidget/DonorRequestWidget.js
+++ donation-app/src/components/DonorRequestWidget/DonorRequestWidget.js
@@ -19,64 +19,57 @@ export default {
   computed: {},
   mounted() {
     this.mySpinner.val = true;
-    // Retrieve request data from firebase
-    db.collection("Requests").get().then((query) => {
-        query.forEach((doc) => {
-          var d_title = doc.id.split(/-(.+)/);
 
-          // only logged in user's requests
-          if (d_title[0] == this.user_id) {
-            // iterate through all charities to find the specified one and pull info from firebase
-            var charity_info = {};
-            db.collection("Charities").get().then((query) => {
-                query.forEach((doc) => {
-                  if (doc.data().email.split("@")[0] == d_title[1]) {
-                    charity_info = {
-                      name: doc.data().name,
-                      phone: doc.data().phone,
-                      address: doc.data().address,
-                      link: doc.data().link,
-                    };
-                  }
-                });
-              })
-              .then(() => {
-                let d = doc.data();
-                this.parseRequest(
-                  d.items,
-                  d_title[0],
-                  charity_info,
-                  d.status,
-                  d.timestamp
-                );
-              });
-          }
-        });
-      })
-      .then(() => {
-        this.mySpinner.val = false;
+    db.collection("Requests").get().then((query) => {
+      query.forEach((doc) => {
+        let d_user = doc.id.split(/-(.+)/)[0];
+        let d_charity = doc.id.split(/-(.+)/)[1];
+
+        // only logged in user's requests
+        if (d_user == this.user_id) {
+          let charity_info = {};
+          db.collection("Charities").doc(d_charity).get().then((doc) => {
+            charity_info = {
+              name: doc.data().name,
+              phone: doc.data().phone,
+              address: doc.data().address,
+              link: doc.data().link,
+            };
+          })
+          .then(() => {
+            let d = doc.data();
+            this.parseRequest(
+              d.items,
+              charity_info,
+              d.status,
+              d.timestamp
+            );
+          });
+        }
+
       });
+    })
+    .then(() => {
+      this.mySpinner.val = false;
+    });
+
+    this.parseHistory();
   },
   methods: {
-    parseRequest(form_data, user, charity, pstatus, time) {
-      // skip requests from other users
-      if (this.user_id == user) {
-        let request = {
-          id: this.id,
-          status: pstatus,
-          timestamp: time,
-          charity: charity,
-          donationLabel: "Donation to " + charity.name,
-          formData: form_data, // TODO consider removing this
-        };
-        this.id += 1;
+    parseRequest(form_data, charity, pstatus, time) {
+      let request = {
+        id: this.id,
+        status: pstatus,
+        timestamp: time,
+        charity: charity,
+        donationLabel: "Donation to " + charity.name,
+        formData: form_data, // TODO consider removing this
+      };
+      this.id += 1;
 
-        if (pstatus == "Pending") this.pendingRequests.push(request);
-        else this.parseHistory();
-      }
+      if (pstatus == "Pending") this.pendingRequests.push(request);
     },
     parseHistory(){
-      this.user_id
       db.collection("History").get().then((query) => {
         query.forEach((doc) => {
           let d = doc.data();
